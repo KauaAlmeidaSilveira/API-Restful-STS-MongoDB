2572b2430f65799ddf7e08cf48ad539052f68ff6
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2021-12-07 12:12:13 -0300

    Project created

---
+++ src/main/java/com/cursoJavaUdemy/ProjectSTSMongoDB/ProjectStsMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.cursoJavaUdemy.ProjectSTSMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectStsMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectStsMongoDbApplication.class, args);
+	}
+
+}
