a89eee2afbf4603ca9275d7937d25bbae8811870
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2021-12-07 20:07:37 -0300

    Getting a user by id

--- src/main/java/com/cursoJavaUdemy/ProjectSTSMongoDB/resources/UserResource.java
+++ src/main/java/com/cursoJavaUdemy/ProjectSTSMongoDB/resources/UserResource.java
@@ -5,8 +5,9 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursoJavaUdemy.ProjectSTSMongoDB.domain.User;
@@ -20,7 +21,7 @@ public class UserResource {
 	@Autowired
 	UserService service;
 	
-	@GetMapping
+	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<UserDTO>> findAll(){
 	
 		List<User> list = service.findAll();
@@ -29,4 +30,11 @@ public class UserResource {
 		
 		return ResponseEntity.ok().body(listDTO);
 	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+ 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+
 }
--- src/main/java/com/cursoJavaUdemy/ProjectSTSMongoDB/services/UserService.java
+++ src/main/java/com/cursoJavaUdemy/ProjectSTSMongoDB/services/UserService.java
@@ -1,23 +1,29 @@
 package com.cursoJavaUdemy.ProjectSTSMongoDB.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cursoJavaUdemy.ProjectSTSMongoDB.domain.User;
 import com.cursoJavaUdemy.ProjectSTSMongoDB.repositories.UserRepository;
+import com.cursoJavaUdemy.ProjectSTSMongoDB.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
-	
-	//O @Autowired ja instancia o objeto automaticamente
+	// O @Autowired ja instancia o objeto automaticamente
 	@Autowired
 	UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
 }
