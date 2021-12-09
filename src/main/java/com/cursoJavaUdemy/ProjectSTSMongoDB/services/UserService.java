daaae1bd7363eff5efbd83524ada1d8240b28aa9
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2021-12-09 11:48:11 -0300

    Insertion of a user with POST

--- src/main/java/com/cursoJavaUdemy/ProjectSTSMongoDB/services/UserService.java
+++ src/main/java/com/cursoJavaUdemy/ProjectSTSMongoDB/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cursoJavaUdemy.ProjectSTSMongoDB.domain.User;
+import com.cursoJavaUdemy.ProjectSTSMongoDB.dto.UserDTO;
 import com.cursoJavaUdemy.ProjectSTSMongoDB.repositories.UserRepository;
 import com.cursoJavaUdemy.ProjectSTSMongoDB.services.exception.ObjectNotFoundException;
 
@@ -26,4 +27,11 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
 }
