3330d78cbc0fe0732925b68c62372bd0bddeff6a
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2021-12-09 16:21:32 -0300

    Getting a post by id

---
+++ src/main/java/com/cursoJavaUdemy/ProjectSTSMongoDB/services/PostService.java
@@ -0,0 +1,25 @@
+package com.cursoJavaUdemy.ProjectSTSMongoDB.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursoJavaUdemy.ProjectSTSMongoDB.domain.Post;
+import com.cursoJavaUdemy.ProjectSTSMongoDB.repositories.PostRepository;
+import com.cursoJavaUdemy.ProjectSTSMongoDB.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	// O @Autowired ja instancia o objeto automaticamente
+	@Autowired
+	PostRepository repository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	
+}
